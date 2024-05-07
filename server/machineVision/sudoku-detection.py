db6f6eb157936d2882ccb242ce1396c4db2b2dc1
Atro Karvinen <atrok@hotmail.com>
2024-05-07 14:19:44 +0300

    added new test images

--- server/machineVision/sudoku-detection.py
+++ server/machineVision/sudoku-detection.py
@@ -2,18 +2,27 @@ from sudokuDetector import SudokuDetector
 
 detector = SudokuDetector()
 
-# Sudoku 1
-sudoku = detector.detect(
-    "C:/Users/atro.karvinen/source/repos/2-omat-projektit/pydoku/server/machineVision/testImages/sudoku_with_noise.JPG")
 
-print("detected sudoku: " + sudoku)
+def test_detect_sudoku(file_path: str, expected_sudoku: str):
+    base_path = "C:/Users/atro.karvinen/source/repos/2-omat-projektit/pydoku/server/machineVision/testImages/"
+    full_path = base_path + file_path
+    sudoku = detector.detect(full_path)
+    print("detected sudoku: " + sudoku)
+    print("expected sudoku: " + expected_sudoku)
+
+
+# Sudoku 1
 expected_sudoku = "7...9..126.5....4.2..7.........7.........927...325.9.1.68.2.....4..15.6..9.6.352."
-print("expected sudoku: " + expected_sudoku)
+test_detect_sudoku("sudoku_with_noise.JPG", expected_sudoku)
 
 # Sudoku 2
-sudoku = detector.detect(
-    "C:/Users/atro.karvinen/source/repos/2-omat-projektit/pydoku/server/machineVision/testImages/example_sudoku_hard.JPG")
-
-print("detected sudoku: " + sudoku)
 expected_sudoku = "8321..4..7....921.1...48.5..8...7.3161...4...2..8...4...9....85.......6......237."
-print("expected sudoku: " + expected_sudoku)
+test_detect_sudoku("example_sudoku_hard.JPG", expected_sudoku)
+
+# Sudoku 3
+expected_sudoku = "....873.9...9.6....45........48..6.528.....915.6..17........56....3.8...4.856...."
+test_detect_sudoku("screenshot_dark_mode.jpg", expected_sudoku)
+
+# Sudoku 4
+expected_sudoku = "....873.9...9.6....45........48..6.528.....915.6..17........56....3.8...4.856...."
+test_detect_sudoku("screenshot_light_mode.jpg", expected_sudoku)
