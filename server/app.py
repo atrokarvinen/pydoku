74ffd105440f86d7e077acf55fd46e303da51b2f
Atro Karvinen <atrok@hotmail.com>
2024-05-13 13:17:39 +0300

    removed unused routes

--- server/app.py
+++ server/app.py
@@ -64,69 +64,6 @@ def get_sudoku_notes():
     return board.serialize()
 
 
-@app.route("/sudoku/row-scan", methods=["POST"])
-def scan_sudoku_rows():
-    sudoku_json = request.get_json()
-    mapper = SudokuMapper()
-    sudoku = mapper.map_from_json(sudoku_json)
-    board = sudoku.board
-    eliminations = sudoku.scan_rows(board)
-    return serialize_eliminations(eliminations)
-
-
-@app.route("/sudoku/column-scan", methods=["POST"])
-def scan_sudoku_columns():
-    sudoku_json = request.get_json()
-    mapper = SudokuMapper()
-    sudoku = mapper.map_from_json(sudoku_json)
-    board = sudoku.board
-    eliminations = sudoku.scan_columns(board)
-    return serialize_eliminations(eliminations)
-
-
-@app.route("/sudoku/box-scan", methods=["POST"])
-def scan_sudoku_boxes():
-    sudoku_json = request.get_json()
-    mapper = SudokuMapper()
-    sudoku = mapper.map_from_json(sudoku_json)
-    board = sudoku.board
-    eliminations = sudoku.scan_boxes(board)
-    return serialize_eliminations(eliminations)
-
-
-@app.route("/sudoku/all-scan", methods=["POST"])
-def scan_sudoku_all():
-    sudoku_json = request.get_json()
-    mapper = SudokuMapper()
-    sudoku = mapper.map_from_json(sudoku_json)
-    board = sudoku.board
-    row_eliminations = sudoku.scan_rows(board)
-    column_eliminations = sudoku.scan_columns(board)
-    box_eliminations = sudoku.scan_boxes(board)
-    eliminations = row_eliminations + column_eliminations + box_eliminations
-    return serialize_eliminations(eliminations)
-
-
-@app.route("/sudoku/single-candidate", methods=["POST"])
-def get_single_candidates():
-    sudoku_json = request.get_json()
-    mapper = SudokuMapper()
-    sudoku = mapper.map_from_json(sudoku_json)
-    board = sudoku.board
-    single_candidates = sudoku.get_single_candidates(board)
-    return [square.serialize() for square in single_candidates]
-
-
-@app.route("/sudoku/is-solved", methods=["POST"])
-def check_sudoku_solved():
-    sudoku_json = request.get_json()
-    mapper = SudokuMapper()
-    sudoku = mapper.map_from_json(sudoku_json)
-    board = sudoku.board
-    error_squares = sudoku.is_solved(board)
-    return [square.serialize() for square in error_squares]
-
-
 @app.route("/sudoku/solve", methods=["POST"])
 def solve_sudoku():
     sudoku_json = request.get_json()
