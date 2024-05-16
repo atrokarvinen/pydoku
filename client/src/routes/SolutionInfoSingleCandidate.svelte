e182e476340112ee044c3df5fc91f1a3d040cc3c
Atro Karvinen <atrok@hotmail.com>
2024-05-16 10:44:29 +0300

    trimmed single candidate text

--- client/src/routes/SolutionInfoSingleCandidate.svelte
+++ client/src/routes/SolutionInfoSingleCandidate.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import type { Alignment, SingleCandidate } from '$lib/types/single-candidate';
+	import type { SingleCandidate } from '$lib/types/single-candidate';
 
 	export let step: number;
 	export let candidate: SingleCandidate;
@@ -8,29 +8,12 @@
 	const numToThreeDigits = (num: number) => {
 		return num.toString().padStart(3, '0');
 	};
-
-	const getAlignmentText = (alignment: Alignment) => {
-		switch (alignment) {
-			case 'row':
-				return 'Row';
-			case 'column':
-				return 'Column';
-			case 'box':
-				return 'Box';
-			case 'cell':
-				return 'Cell';
-			default:
-				return `Unknown alignment: ${alignment}`;
-		}
-	};
 </script>
 
 <div>
 	<button on:click={() => candidateClicked(candidate)}>
 		<span>#{numToThreeDigits(step)}</span>
-		<span>{`(${candidate.column + 1}, ${candidate.row + 1})`}</span>
-		<span>{`number: ${candidate.number}`}</span>
 		<span>,</span>
-		<span>Single candidate ({getAlignmentText(candidate.alignment)})</span>
+		<span>Single candidate</span>
 	</button>
 </div>
