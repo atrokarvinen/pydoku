5bb52ffbacc4a17f11dc754a213c1e19e3dbd5b2
Atro Karvinen <atrok@hotmail.com>
2024-05-17 17:00:27 +0300

    added tests for hidden pair

--- server/unitTesting/testHiddenPair.py
+++ server/unitTesting/testHiddenPair.py
@@ -2,21 +2,19 @@ import unittest
 
 from models.board import Board
 from models.numberedNote import NumberedNote
-from techniques.nakedPair import NakedPair
+from techniques.hiddenPair import HiddenPair
 from unitTesting.customAsserts import CustomAsserts
 
 
-class TestNakedPair(unittest.TestCase, CustomAsserts):
+class TestHiddenPair(unittest.TestCase, CustomAsserts):
     def setUp(self) -> None:
-        self.solver = NakedPair()
+        self.solver = HiddenPair()
         self.board = Board(9)
         self.board.initialize_empty()
 
     def test_returns_elimination_for_row(self):
-        self.board.set_notes(0, 0, [1, 2])
-        self.board.set_notes(0, 1, [1, 2])
-        self.board.set_notes(0, 5, [1])
-        self.board.set_notes(0, 6, [2])
+        self.board.set_notes(0, 0, [1, 2, 9])
+        self.board.set_notes(0, 1, [1, 2, 9])
 
         solution = self.solver.get_next_solution(self.board)
 
@@ -25,14 +23,11 @@ class TestNakedPair(unittest.TestCase, CustomAsserts):
             NumberedNote(0, 0, 2), NumberedNote(0, 0, 1),
             NumberedNote(0, 1, 1), NumberedNote(0, 1, 2)])
         self.assertListEqualNoOrder(solution.eliminated_notes, [
-            NumberedNote(0, 5, 1), NumberedNote(0, 6, 2)])
+            NumberedNote(0, 0, 9), NumberedNote(0, 1, 9)])
 
     def test_returns_elimination_for_column(self):
-        self.board.initialize_empty()
-        self.board.set_notes(0, 0, [1, 2])
-        self.board.set_notes(1, 0, [1, 2])
-        self.board.set_notes(5, 0, [1])
-        self.board.set_notes(6, 0, [2])
+        self.board.set_notes(0, 0, [1, 2, 9])
+        self.board.set_notes(1, 0, [1, 2, 9])
 
         solution = self.solver.get_next_solution(self.board)
 
@@ -41,13 +36,11 @@ class TestNakedPair(unittest.TestCase, CustomAsserts):
             NumberedNote(0, 0, 2), NumberedNote(0, 0, 1),
             NumberedNote(1, 0, 1), NumberedNote(1, 0, 2)])
         self.assertListEqualNoOrder(solution.eliminated_notes, [
-            NumberedNote(5, 0, 1), NumberedNote(6, 0, 2)])
+            NumberedNote(0, 0, 9), NumberedNote(1, 0, 9)])
 
     def test_returns_elimination_for_box(self):
-        self.board.set_notes(0, 0, [1, 2])
-        self.board.set_notes(1, 1, [1, 2])
-        self.board.set_notes(0, 1, [1])
-        self.board.set_notes(1, 0, [2])
+        self.board.set_notes(0, 0, [1, 2, 9])
+        self.board.set_notes(1, 1, [1, 2, 9])
 
         solution = self.solver.get_next_solution(self.board)
 
@@ -56,15 +49,12 @@ class TestNakedPair(unittest.TestCase, CustomAsserts):
             NumberedNote(0, 0, 2), NumberedNote(0, 0, 1),
             NumberedNote(1, 1, 1), NumberedNote(1, 1, 2)])
         self.assertListEqualNoOrder(solution.eliminated_notes, [
-            NumberedNote(0, 1, 1), NumberedNote(1, 0, 2)])
+            NumberedNote(0, 0, 9), NumberedNote(1, 1, 9)])
 
     def test_returns_elimination_for_triple(self):
-        self.board.set_notes(0, 0, [1, 2, 3])
-        self.board.set_notes(0, 1, [1, 2, 3])
-        self.board.set_notes(0, 2, [1, 2, 3])
-        self.board.set_notes(0, 5, [1])
-        self.board.set_notes(0, 6, [2])
-        self.board.set_notes(0, 7, [3])
+        self.board.set_notes(0, 0, [1, 2, 3, 9])
+        self.board.set_notes(0, 1, [1, 2, 3, 9])
+        self.board.set_notes(0, 2, [1, 2, 3, 9])
 
         solution = self.solver.get_next_solution(self.board)
 
@@ -78,38 +68,45 @@ class TestNakedPair(unittest.TestCase, CustomAsserts):
             NumberedNote(0, 1, 3),
             NumberedNote(0, 2, 1),
             NumberedNote(0, 2, 2),
-            NumberedNote(0, 2, 3),
-        ])
+            NumberedNote(0, 2, 3)])
         self.assertListEqualNoOrder(solution.eliminated_notes, [
-            NumberedNote(0, 5, 1), NumberedNote(0, 6, 2), NumberedNote(0, 7, 3)])
+            NumberedNote(0, 0, 9), NumberedNote(0, 1, 9), NumberedNote(0, 2, 9)])
 
     def test_returns_elimination_for_partial_triple(self):
-        self.board.set_notes(0, 0, [1, 2])
-        self.board.set_notes(0, 1, [2, 3])
-        self.board.set_notes(0, 2, [1, 3])
-        self.board.set_notes(0, 5, [1])
-        self.board.set_notes(0, 6, [2])
-        self.board.set_notes(0, 7, [3])
+        self.board.set_notes(0, 0, [1, 2, 9])
+        self.board.set_notes(0, 1, [1, 2, 9])
+        self.board.set_notes(0, 2, [1, 2, 9])
 
         solution = self.solver.get_next_solution(self.board)
 
         self.assertNotEqual(solution, None)
         self.assertListEqualNoOrder(solution.causing_notes, [
-            NumberedNote(0, 0, 1),
-            NumberedNote(0, 0, 2),
-            NumberedNote(0, 1, 2),
-            NumberedNote(0, 1, 3),
-            NumberedNote(0, 2, 1),
-            NumberedNote(0, 2, 3),
-        ])
+            NumberedNote(0, 0, 2), NumberedNote(0, 0, 1),
+            NumberedNote(0, 1, 1), NumberedNote(0, 1, 2),
+            NumberedNote(0, 2, 1), NumberedNote(0, 2, 2)])
         self.assertListEqualNoOrder(solution.eliminated_notes, [
-            NumberedNote(0, 5, 1), NumberedNote(0, 6, 2), NumberedNote(0, 7, 3)])
+            NumberedNote(0, 0, 9), NumberedNote(0, 1, 9), NumberedNote(0, 2, 9)])
 
-    def test_returns_none_when_no_pair(self):
-        self.board.set_notes(0, 0, [1, 2])
-        self.board.set_notes(0, 1, [1, 9])
-        self.board.set_notes(0, 5, [1])
-        self.board.set_notes(0, 6, [2])
+    def test_returns_none_for_no_pair(self):
+        self.board.set_notes(0, 0, [1, 2, 9])
+        self.board.set_notes(0, 1, [1, 9, 8])
+        self.board.set_notes(1, 0, [1, 9, 8])
+        self.board.set_notes(1, 1, [1, 9, 8])
+
+        self.board.set_notes(7, 0, [1, 2, 9])
+        self.board.set_notes(7, 1, [1, 9, 8])
+        self.board.set_notes(8, 0, [1, 9, 8])
+        self.board.set_notes(8, 1, [1, 9, 8])
+
+        self.board.set_notes(0, 7, [1, 2, 9])
+        self.board.set_notes(0, 8, [1, 9, 8])
+        self.board.set_notes(1, 7, [1, 9, 8])
+        self.board.set_notes(1, 8, [1, 9, 8])
+
+        self.board.set_notes(7, 7, [1, 2, 9])
+        self.board.set_notes(7, 8, [1, 9, 8])
+        self.board.set_notes(8, 7, [1, 9, 8])
+        self.board.set_notes(8, 8, [1, 9, 8])
 
         solution = self.solver.get_next_solution(self.board)
 
